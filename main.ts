d5ad8762b739c05d500e74d8e0b584694ec8d2f4
eliasmpoutakis-hue <elias_mpoutakis@atlascollege.be>
2025-11-27 10:06:46 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,4 @@
+Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 125)
+basic.pause(2000)
+Maqueen_V5.motorStop(Maqueen_V5.Motors.M2)
+Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
